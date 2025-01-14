6fe380ed8bb0cfb568c81619af3a5d00ef425c98
paulopariz <paulopariz01@gmail.com>
2025-01-13 21:58:04 -0300

    chore: dependencies

---
+++ commitlint.config.ts
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    'header-max-length': [0],
+    'function-rules/header-max-length': [
+      2,
+      'always',
+      (parsed: { header: string }) => {
+        if (parsed.header.length > 100) {
+          return [
+            false,
+            `the commit must not be longer than 100 characters, the current length is ${parsed.header.length}`,
+          ];
+        }
+        if (
+          !parsed.header.match(
+            /^((feat)|(test)|(build)|(chore)|(ci)|(docs)|(fix)|(perf)|(refactor)|(style)): .+/gm,
+          )
+        ) {
+          return [
+            false,
+            `the commit message must follow the pattern "type: message"`,
+          ];
+        }
+        return [true];
+      },
+    ],
+  },
+};
